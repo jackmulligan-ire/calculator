6a2a4f38249d3f3461dd061e5dfbc078f335ef1c
Jack Mulligan <mulligja1@gmail.com>
2021-09-05 16:43:58 +0100

    Calculator styling

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
+const operate = (a, operator, b) => operator(a,b);
+
+let test = operate(1, divide, 4)
+console.log(test)
+    
