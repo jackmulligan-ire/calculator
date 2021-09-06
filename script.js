bbc287e7403e3ad5cf16980a985eafe016cc6087
Jack Mulligan <mulligja1@gmail.com>
2021-09-06 10:02:57 +0100

    Calculator design

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Operator functions
 const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
 const operate = (a, operator, b) => operator(a,b);
-
-let test = operate(1, divide, 4)
-console.log(test)
     
