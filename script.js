15f6c29d4654434ccba5c45e6a177c1df012c1c4
Jack Mulligan <mulligja1@gmail.com>
2021-09-07 14:12:15 +0100

    Chaining logic corrrected

--- script.js
+++ script.js
@@ -9,13 +9,23 @@ const equalsButton = document.querySelector("#equals")
 
 let num1, currentOperation, num2;
 
-const plus = (a,b) => (a + b).toString();
-const subtract = (a,b) => a - b.toString();
-const multiply = (a,b) => a * b.toString();
+const plus = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
 const divide = (a,b) => (a / b).toFixed(2);
 const operate = (a, operator, b) => operator(a,b);
 const storeToMemory = (operation) => {
-    num1 = +display.textContent;
+    // Set condition to be defined and not equal to whatever is in the display
+    if (num1 != undefined && num1 != +display.textContent) {
+        // Store num2 from the display
+        num2 = +display.textContent;
+        // Call operate with num1, currentOperation, num2, store in result
+        result = operate(num1, currentOperation, num2);
+        // Take result of the operation and store it in num 1
+        num1 = result;
+    }
+    // if num 1 IS undefined store num1 from the display
+    else if (num1 === undefined) num1 = +display.textContent;
     display.textContent = "";
     currentOperation = operation;
 };
@@ -31,7 +41,13 @@ multiplyButton.addEventListener('click', () => storeToMemory(multiply))
 divideButton.addEventListener('click', () => storeToMemory(divide))
 equalsButton.addEventListener('click', () => {
     num2 = +display.textContent
-    display.textContent = operate(num1, currentOperation, num2);
+    // Store result of operation in result
+    result = operate(num1, currentOperation, num2);
+    // Let num1 = result
+    // Let the display show the result
+    display.textContent = result;
+    num1 = result; // Storing if user performing new operation with result
+
 })
 clearButton.addEventListener('click', () => {
     display.textContent = "";
