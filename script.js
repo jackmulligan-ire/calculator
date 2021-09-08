4e08dd592dfb181c620b419a74b239fd460ed1e4
Jack Mulligan <mulligja1@gmail.com>
2021-09-08 10:48:03 +0100

    Comment clean up

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const divideButton = document.querySelector("#divide");
 const equalsButton = document.querySelector("#equals")
 
 let num1, currentOperation, num2; 
-//Define an array called activeKeystrokes.
 let activeKeystrokes = [];
 
 const plus = (a,b) => a + b;
@@ -17,35 +16,24 @@ const multiply = (a,b) => a * b;
 const divide = (a,b) => (a / b).toFixed(2);
 const operate = (a, operator, b) => operator(a,b);
 const storeToMemory = (operation) => {
-    // Set condition to be defined and not equal to whatever is in the display
-    if (num1 != undefined && num1 != +display.textContent) {
-        // Store num2 from the display
+    if (num1 != undefined && num1 != +display.textContent) { //Chaining operations
         num2 = +display.textContent;
-        // Call operate with num1, currentOperation, num2, store in result
         result = operate(num1, currentOperation, num2);
-        // Take result of the operation and store it in num 1
-        num1 = result;
-        // Update display to be num1
+        num1 = result; // For new calc after equals pressed
         display.textContent = num1;
     }
-    // if num 1 IS undefined store num1 from the display
-    else if (num1 === undefined) {
-        num1 = +display.textContent;
-    }
+    else if (num1 === undefined) num1 = +display.textContent; //Very first number
     currentOperation = operation;
-    // Wipe activeKeystrokes
     activeKeystrokes = [];
 };
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        // If the content of the display is the same as num1
-        // Update if block so that activeKeystrokes has to be emptied
+        // Wiping for new number
         if (+display.textContent === num1 && activeKeystrokes.length === 0) {
             display.textContent = ""
         }
         display.textContent += button.textContent;
-        // Entry added to the activeKeystrokes array
         activeKeystrokes.push(button.textContent);
     })
 })
@@ -55,13 +43,9 @@ multiplyButton.addEventListener('click', () => storeToMemory(multiply))
 divideButton.addEventListener('click', () => storeToMemory(divide))
 equalsButton.addEventListener('click', () => {
     num2 = +display.textContent
-    // Store result of operation in result
     result = operate(num1, currentOperation, num2);
-    // Let num1 = result
-    // Let the display show the result
     display.textContent = result;
-    num1 = result; // Storing if user performing new operation with result
-    // Wipe activeKeystrokes
+    num1 = result; // For new calc after equals pressed
     activeKeystrokes = [];
 
 })
