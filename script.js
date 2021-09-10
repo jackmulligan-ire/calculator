d701bce0b96dfe61090463ca389108bf8c9d6090
Jack Mulligan <mulligja1@gmail.com>
2021-09-10 14:06:22 +0100

    Reversing decimal freeze

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const equalsButton = document.querySelector("#equals")
 let num1, currentOperation, num2; 
 let activeKeystrokes = [];
 let equalsFrozen = false;
+let decimalFrozen = false;
 
 const plus = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
-
 const divide = (a,b) => {
     if (a % b === 0) return a / b
     else return (a / b).toFixed(2)   
@@ -25,16 +25,30 @@ const calculate = (a, operator, b) => {
     display.textContent = operationResult;
     equalsFrozen = true;
     return operationResult;
-}
+};
 
 const updateMemory = (operation) => {
     if (num1 != undefined && num1 != +display.textContent) { // Chaining operations
         num2 = +display.textContent;
         operationResult = calculate(num1, currentOperation, num2);
         num1 = operationResult; // For next calc, if continuing to chain
-    } else if (num1 === undefined) num1 = +display.textContent; // Very first number
+    } 
+    else if (num1 === undefined) num1 = +display.textContent; // Very first number
     currentOperation = operation;
     activeKeystrokes = [];
+    decimalFrozen = false;
+};
+
+const updateDisplay = (button) => {
+    if (button.getAttribute('id') != "decimal") {
+        display.textContent += button.textContent;
+        activeKeystrokes.push(button.textContent);
+    }
+    else if (button.getAttribute('id') === "decimal" && decimalFrozen === false) {
+        display.textContent += button.textContent;
+        activeKeystrokes.push(button.textContent);
+        decimalFrozen = true;
+    }
 };
 
 plusButton.addEventListener('click', () => updateMemory(plus))
@@ -47,14 +61,15 @@ numberButtons.forEach(button => {
         if (equalsFrozen) {
             display.textContent = ""
             equalsFrozen = false;
-        } else if (+display.textContent === num1 && activeKeystrokes.length === 0) {
+        } 
+        else if (+display.textContent === num1 && activeKeystrokes.length === 0) {
             display.textContent = ""
-        } else if (display.textContent === "Error") {
+        } 
+        else if (display.textContent === "Error") {
             display.textContent = ""
             num1 = undefined;
         }
-        display.textContent += button.textContent;
-        activeKeystrokes.push(button.textContent);
+        updateDisplay(button)
     })
 })
 
@@ -62,12 +77,14 @@ equalsButton.addEventListener('click', () => {
     if (num1 === undefined) {
         num1 = "Error";
         display.textContent = num1;
-    } else if (equalsFrozen === false) {
+    } 
+    else if (equalsFrozen === false) {
         num2 = +display.textContent;
         operationResult = calculate(num1, currentOperation, num2);
         num1 = operationResult; // For next operation after equals pressed
     }
     activeKeystrokes = [];
+    decimalFrozen = false;
 })
 
 clearButton.addEventListener('click', () => {
@@ -77,4 +94,5 @@ clearButton.addEventListener('click', () => {
     currentOperation = undefined;
     activeKeystrokes = [];
     equalsFrozen = false;
+    decimalFrozen = false;
 })
